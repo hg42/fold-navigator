66100febe5334d73bab6c0d3ba0486fd6d92fe63
hg42@gmx.net <hg42@gmx.net>
2017-02-26 19:27:39 +0100

    add specs from atom template

---
+++ spec/navigator-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NavigatorView from '../lib/navigator-view';
+
+describe('NavigatorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
